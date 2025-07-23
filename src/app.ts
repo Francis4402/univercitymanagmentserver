aa409f8d0a628827e5bc1f4991ee34ec8514a2ad
Francis4402 <francism4402@gmail.com>
2025-07-23 15:58:54 +0600

    server route update

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({ origin: ['*'], credentials: true }));
